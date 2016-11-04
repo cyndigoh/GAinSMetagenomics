066ea942693f1bda8b3972250caa4c492a3ad2b8
cyndigoh <cyndi.goh@gmail.com>
2016-11-04 17:14:02 +0000

    with patrick's input

--- stampy.sh
+++ stampy.sh
@@ -1,26 +1,20 @@
-#/well/jknight/cyndi/scripts/stampy/runstampy.sh
-#Script to run stampy.sh with fastq input
+#Usage: 
+#LOCA="/well/jknight/cyndi"
+#LOCB="${LOCA}/metagenomics_data/P160597"
+#qsub -v samplelist=${LOCB}/index/pool2.txt,refgenome=${LOCA}/references/probes/probesall,pathtofile=${LOCB}/A-rawdata/pool2,outputpath=${LOCB}/B-mapped/pool2/probes ${LOCA}/scripts/stampy/test.sh
+
+#Script to run stampy with fastq input
 #!/usr/bin/env bash
 #$ -cwd
 #$ -q short.qc
 #$ -P jknight.prjc
-#$ -N runstampy
+#$ -N stampy
 #$ -o /well/jknight/cyndi/qsub_logs
 #$ -e /well/jknight/cyndi/qsub_logs
 #$ -pe shmem 2
 #$ -V
 
-/well/jknight/cyndi/metagenomics_data/P160597/C-scripts/test/stampy.sh /well/jknight/cyndi/metagenomics_data/P160597/C-scripts/test/samplelist.txt /well/jknight/cyndi/references/probes/probesall /well/jknight/cyndi/metagenomics_data/P160597/A-rawdata/pool2 /well/jknight/cyndi/metagenomics_data/P160597/B-mapped/pool2/probes
-
-#/well/jknight/cyndi/scripts/stampy/stampy.sh
-#Script to run Stampy with fastq input
-# Defining the arguments
-samplelist=$1
-refgenome=$2
-pathtofile=$3
-outputpath=$4
-
-# this export PATH is needed for samtools
+#this export PATH is needed for samtools
 export PATH=/apps/well/samtools/0.1.19/bin:${PATH}
 export PATH=/usr/local/genetics/bin:${PATH}
 
