8ada22bfc8133e5c0822309124e05d7fd38f3b16
cyndigoh <cyndi.goh@gmail.com>
2016-11-10 15:52:33 +0000

    Update markduplicates.sh

--- markduplicates.sh
+++ markduplicates.sh
@@ -21,3 +21,25 @@ while read line; do
       M=${outputpath}/${line}_metrics.txt \
       ASSUME_SORTED=true
 done < ${samplelist}
+
+#noduplicates.sh - counts the number of reads mapping to a reference which are NOT duplicates
+#Usage:
+#qsub -o /well/jknight/cyndi/metagenomics_data/P160597/D-analysis/markduplicates/hiseq/strep/summary.tsv -v samplelist=/well/jknight/cyndi/metagenomics_data/P160597/index/hiseq.txt,path=/well/jknight/cyndi/metagenomics_data/P160597/D-analysis/markduplicates/hiseq/strep /well/jknight/cyndi/scripts/samtools/noduplicates.sh 
+
+#!/usr/bin/env bash
+#$ -cwd
+#$ -q short.qc
+#$ -P jknight.prjc
+#$ -N noduplicates
+#$ -e /well/jknight/cyndi/qsub_logs
+#$ -pe shmem 2
+#$ -V
+
+printf "sampleindex\ttotalreads\ttotalmapped\tmappednotduplicate\n"
+FILE=${samplelist}
+while read line; do
+        totalreads=`samtools view -c ${path}/${line}_marked_duplicates.bam`
+        totalmapped=`samtools view -F4 -c ${path}/${line}_marked_duplicates.bam`
+        mappednotduplicate=`samtools view -F1028 -c ${path}/${line}_marked_duplicates.bam`
+        printf "${line}\t${totalreads}\t${totalmapped}\t${mappednotduplicate}\n"
+done < ${samplelist}
